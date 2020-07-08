6977a2bb43eeee031efeec14354ee293bf463815
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-07-08 20:57:29 +0900

    btoa -> buffer

--- lib/storage.ts
+++ lib/storage.ts
@@ -5,7 +5,7 @@ const STORAGE_PATH = (name: string) => `share/${name}`
 export async function uploadImage(name: string, image: Buffer) {
   const credentials =
     process.env.GCLOUD_CREDENTIALS &&
-    JSON.parse(btoa(process.env.GCLOUD_CREDENTIALS))
+    JSON.parse(Buffer.from(process.env.GCLOUD_CREDENTIALS).toString('base64'))
   const storage = new Storage({ credentials })
   const bucket = storage.bucket('atcoder-aisho-shindan')
   const file = bucket.file(STORAGE_PATH(name))
