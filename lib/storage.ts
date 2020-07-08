be76779bb72dcb8580e0f829a0729023dca40d75
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-07-08 20:33:46 +0900

    + credentials

--- lib/storage.ts
+++ lib/storage.ts
@@ -3,7 +3,9 @@ import { Storage } from '@google-cloud/storage'
 const STORAGE_PATH = (name: string) => `share/${name}`
 
 export async function uploadImage(name: string, image: Buffer) {
-  const storage = new Storage({ keyFilename: process.env.GCLOUD_CREDENTIALS })
+  const storage = new Storage({
+    credentials: JSON.parse(process.env.GCLOUD_CREDENTIALS),
+  })
   const bucket = storage.bucket('atcoder-aisho-shindan')
   const file = bucket.file(STORAGE_PATH(name))
   await file.save(image, { metadata: { contentType: 'image/png' } })
